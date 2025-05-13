5fce7231caecb124fa63697d995f30e2b74ef280
who-sam <hossam.samy.rashed@gmail.com>
2025-05-13 22:43:03 +0300

    grouped the classes

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH/2 ,SCREEN_HEIGHT/2)
 
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH/2 ,SCREEN_HEIGHT/2)
     print("Starting Asteroids!")
     
     while True:
@@ -20,9 +24,13 @@ def main():
                 return
 
         # pygame.Surface.fill(screen,(0,0,0))
-        player.update(dt)
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
