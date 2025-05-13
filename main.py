447630722f61af6869a7009b5ab186e74139f270
whosam-lap <hossam.samy.rashed@gmail.com>
2025-05-13 20:08:13 +0300

    made the game run on 60 fps

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0,0,0))
+        # pygame.Surface.fill(screen,(0,0,0))
+        screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 
