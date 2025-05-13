f485df1656653f843cafe13bfe9e356dad9433fe
whosam-lap <hossam.samy.rashed@gmail.com>
2025-05-13 21:10:55 +0300

    ..

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 
 def main():
@@ -10,6 +12,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2 ,SCREEN_HEIGHT/2)
     print("Starting Asteroids!")
     while 1:
         for event in pygame.event.get():
@@ -17,6 +20,7 @@ def main():
                 return
         # pygame.Surface.fill(screen,(0,0,0))
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
