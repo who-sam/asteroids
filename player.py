ee88bca87fa317f1999e019b114faeae3010809e
who-sam <hossam.samy.rashed@gmail.com>
2025-05-13 21:26:32 +0300

    added the player

--- player.py
+++ player.py
@@ -3,12 +3,12 @@ from constants import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
-        super().__init__(self, x, y, PLAYER_RADIUS)
+        super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
     def draw(self, screen):
-        pygame.draw.polygon(screen , "white" , self.triangle() , width = 2 ):
-            pass)
+        pygame.draw.polygon(screen , "white" , self.triangle() , width = 2 )
+        pass
 
 
     # in the player class
