acb7506fcbf7e488d63697c45eacdcf501589193
whosam-lap <hossam.samy.rashed@gmail.com>
2025-05-13 19:39:56 +0300

    initiated screen

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ from constants import *
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen,(0,0,0))
+        pygame.display.flip()
 
 
 
