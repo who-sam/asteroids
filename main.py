e581deaf1d49d4138a762bf94104da0358670d96
whosam-lap <hossam.samy.rashed@gmail.com>
2025-05-13 18:52:57 +0300

    added constants.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+
+
+if __name__ =="__main__" :
+    main()
